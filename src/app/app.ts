cf8b6d6b449b31c76ecd663c085ba4504631fc09
Brikuus <rosmann.henri16@gmail.com>
2025-09-30 16:21:44 +0200

    title change

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('mon-super-projet');
+  protected readonly title = signal("brique's e-shop");
 }
