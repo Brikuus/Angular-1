af2978b64a0eb12c60d40e4940aedd6bd70f6955
Brikuus <rosmann.henri16@gmail.com>
2025-10-01 11:26:52 +0200

    component finished

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProductCard} from './features/products/components/product-card/product-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal("brique's e-shop");
+  protected readonly h1 = signal("brique's e-shop");
 }
